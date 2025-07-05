2523f2ef3ee58c4b0f365893b3aeac304fa6b808
mfauzi77 <mmfauzi77@gmail.com>
2025-07-05 11:21:27 +0700

    restore local changes after rebase

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // Ganti 'nama-repo' dengan nama repository GitHub kamu
 export default defineConfig({
+  base: '/paudhi7/', // Ganti dengan nama repository kamu
   plugins: [react(), tailwindcss()],
 });
